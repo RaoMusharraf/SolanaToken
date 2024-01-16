7b42b0d3a489497c2d2e7a2453ca8e7cf23260ce
RaoMusharraf <mmusharraf829@gmail.com>
2024-01-16 16:36:05 +0500

    SRC20

---
+++ mint.ts
@@ -0,0 +1,36 @@
+import { percentAmount, generateSigner, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi'
+import { TokenStandard, createAndMint } from '@metaplex-foundation/mpl-token-metadata'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import  "@solana/web3.js";
+import secret from './id.json';
+
+const umi = createUmi('https://api.devnet.solana.com'); //Replace with your QuickNode RPC Endpoint
+
+const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secret));
+const userWalletSigner = createSignerFromKeypair(umi, userWallet);
+
+const metadata = {
+    name: "Octaloop Technology",
+    symbol: "Octa",
+    uri: "https://ipfs.io/ipfs/Qmb6nYGLxXdDpf3iPYKBtN2f7bZvgu7LnZuVmFqvr5xNgp",
+};
+
+const mint = generateSigner(umi);
+umi.use(signerIdentity(userWalletSigner));
+umi.use(mplCandyMachine())
+
+createAndMint(umi, {
+    mint,
+    authority: umi.identity,
+    name: metadata.name,
+    symbol: metadata.symbol,
+    uri: metadata.uri,
+    sellerFeeBasisPoints: percentAmount(0),
+    decimals: 9,
+    amount: 1844674407370955161,
+    tokenOwner: userWallet.publicKey,
+    tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm(umi).then(() => {
+    console.log("Successfully minted 1 million tokens (", mint.publicKey, ")");
+});
